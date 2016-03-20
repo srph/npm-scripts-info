90b91ae0133c1465e42c1d741cd43907129f3485
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-03-20 23:34:15 +0200

    feat(lib): add default script descriptions

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -4,14 +4,63 @@ var rewire = require('rewire');
 var info = rewire('../lib');
 
 describe('npm-script-info', function() {
-  it('should extract `scripts-info` from package.json', function() {
-    var dummy = {};
+  var revert;
+  var pkgJSON;
 
-    var revert = info.__set__('pkg', function() {
-      return { 'scripts-info': dummy };
+  before(function() {
+    revert = info.__set__('pkg', function() {
+      return pkgJSON;
     });
+  });
+
+  after(function() {
+    revert();
+  });
+
+  it('should throw exception if no description found', function() {
+    pkgJSON = {};
+
+    expect(function() {
+      info();
+    }).to.throw(Error, "Oops, there's no info about your scripts. Consider fleshing out the `scripts-info` property in your `package.json`");
+  });
+
+  it('should extract `scripts-info` from package.json', function() {
+    var dummy = {
+      foo: 'bar',
+    };
+
+    pkgJSON = { 'scripts-info': dummy };
 
     expect(info()).to.eql(dummy);
-    revert();
+  });
+
+  it('should add default script info if missing in `scripts-info`', function() {
+    pkgJSON = {
+      scripts: {
+        info: 'npm-scripts-info',
+        start: 'node index',
+      },
+    };
+
+    expect(info()).to.eql({
+      info: 'Display information about the scripts',
+      start: 'Kickstart the application',
+    });
+  });
+
+  it('should not override script info with default description', function() {
+    pkgJSON = {
+      scripts: {
+        start: 'node index',
+      },
+      'scripts-info': {
+        start: 'Custom start description'
+      }
+    };
+
+    expect(info()).to.eql({
+      start: 'Custom start description',
+    });
   });
 });
